f06054af3887d0c6cc7b169001effa1185e46f43
Michael Hancock <michael.hancock@protonmail.com>
2016-05-23 22:16:54 +0100

    Completed KSP section and minor tweaks
    Adjusted margin spacing between elements in kerbal section. Fixed
    noscript bug by removing scoped attribute. Added close condition to
    slide menu, occurs when any section element is clicked. Moved logo svg
    in mobile mode to be closer to top left corner.

--- assets/scripts/work_control.js
+++ assets/scripts/work_control.js
@@ -62,6 +62,11 @@ function events() {
         closeMenu();
     });
 
+    //Close menu if anything other than the menu is clicked
+    $('section').click(function() {
+        closeMenu();
+    });
+
     function openMenu() {
         //Open slide in menu
         if (isOpen === false) {
